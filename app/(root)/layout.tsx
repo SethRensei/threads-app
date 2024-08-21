cb15623ca06399f65500fd5299cbca3045f07442
SethRensei <sitaseth@yahoo.com>
2024-08-21 03:23:25 +0100

    Build topbar and leftsidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+
 import "../globals.css";
+import Topbar from "@/components/shared/Topbar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Bottombar from "@/components/shared/Bottombar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +22,27 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
-            <body className={inter.className}>{children}</body>
-        </html>
+        <ClerkProvider
+            appearance={{
+                baseTheme: dark,
+            }}
+        >
+            <html lang="en">
+                <body className={inter.className}>
+                    <Topbar />
+
+                    <main className="flex flex-row">
+                        <LeftSidebar />
+                        <section className="main-container">
+                            <div className="w-full max-w-4xl">{children}</div>
+                        </section>
+                        {/* @ts-ignore */}
+                        <RightSidebar />
+                    </main>
+
+                    <Bottombar />
+                </body>
+            </html>
+        </ClerkProvider>
     );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,18 +1,8 @@
-import { ClerkProvider, UserButton } from "@clerk/nextjs";
-
 export default function Home() {
     return (
-        <ClerkProvider>
-            <div>
-                <UserButton />
-                <h1>Home</h1>
-                <p>Welcome to Threads!</p>
-                <p>
-                    This is a simple Next.js app with a Clerk authentication
-                    system. You can sign up, log in, and see your profile
-                    information.
-                </p>
-            </div>
-        </ClerkProvider>
+        <div className="container">
+            <h1>Welcome to Threads</h1>
+            <p>Une application de fils sur Next.js</p>
+        </div>
     );
 }
